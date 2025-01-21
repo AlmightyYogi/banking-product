ea268a28a678c1658faa116d2138463d7dc93bad
Frayogi <nisaafriani957@gmail.com>
2025-01-21 16:34:42 +0700

    Ganti port dari 3000 jadi 3001

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const app = require('./app');
 require('dotenv').config();
 
-// Menentukan port yang akan digunakan, default ke 3000 jika tidak ada di .env
-const PORT = process.env.PORT || 3000;
+// Menentukan port yang akan digunakan, default ke 3001 jika tidak ada di .env
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
