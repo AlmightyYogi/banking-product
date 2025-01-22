df8c0c06f3a77294a116ad16bd22a188291c8e74
Frayogi <nisaafriani957@gmail.com>
2025-01-22 16:04:32 +0700

    Tambahkan route product by id

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,6 +4,7 @@ const ProductController = require('../controllers/productController');
 const router = express.Router();
 
 router.post('/', ProductController.createProduct);
+router.get('/:id', ProductController.getProductById);
 router.put('/:id', ProductController.updateProduct);
 router.get('/', ProductController.getProducts);
 
