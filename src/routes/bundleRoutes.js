6286157dbedb84a06b3a3727dadee52900489b40
Frayogi <nisaafriani957@gmail.com>
2025-01-22 16:04:09 +0700

    Tambahkan route bundle by id

--- src/routes/bundleRoutes.js
+++ src/routes/bundleRoutes.js
@@ -4,6 +4,7 @@ const BundleController = require('../controllers/bundleController');
 const router = express.Router();
 
 router.post('/', BundleController.createBundle);
+router.get('/:id',BundleController.getBundleById);
 router.put('/:id', BundleController.updateBundle)
 router.get('/', BundleController.getBundles);
 
