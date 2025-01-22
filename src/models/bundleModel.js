713d4344432e3f67562c683a8cfffb7745bbae78
Frayogi <nisaafriani957@gmail.com>
2025-01-22 16:03:05 +0700

    Menambah model tangkap bundle by id dan tambahkan error handling

--- src/models/bundleModel.js
+++ src/models/bundleModel.js
@@ -2,25 +2,47 @@ const db = require('../config/db')
 
 // Menambah bundle menggunakan query ke table bundles
 exports.createBundle = async (bundle) => {
-    const [rows] = await db.query(
-        `INSERT INTO bundles (name, product_id, price, stock, description)
-         VALUES (?, ?, ?, ?, ?)`,
-        [bundle.name, bundle.product_id, bundle.price, bundle.stock, bundle.description]
-    );
-    return rows;
+    try {
+        const [rows] = await db.query(
+            `INSERT INTO bundles (name, product_id, price, stock, description)
+            VALUES (?, ?, ?, ?, ?)`,
+            [bundle.name, bundle.product_id, bundle.price, bundle.stock, bundle.description]
+        );
+        return rows;
+    } catch (error) {
+        throw new Error("Failed to create bundle");
+    }
 };
 
 // Mengambil semua data bundle dari table bundles
 exports.getBundles = async () => {
-    const [rows] = await db.query(`SELECT * FROM bundles`);
-    return rows;
+    try {
+        const [rows] = await db.query(`SELECT * FROM bundles`);
+        return rows;
+    } catch (error) {
+        throw new Error('Failed to fetch products');
+    }
+};
+
+// Mengambil data berdasarkan id dari table bundles
+exports.getBundleById = async (id) => {
+    try {
+        const [rows] = await db.query(`SELECT * FROM bundles WHERE id = ?`, [id]);
+        return rows;
+    } catch(error) {
+        throw new Error('Failed to fetch bundle by id');
+    }
 };
 
 // Meng-update data bundle dari table bundles
 exports.updateBundle = async (id, bundle) => {
-    const [rows] = await db.query(
-        `UPDATE bundles SET name = ?, product_id = ?, price = ?, stock = ?, description = ? WHERE id = ?`,
-        [bundle.name, bundle.product_id, bundle.price, bundle.stock, bundle.description, id]
-    );
-    return rows;
+    try {
+        const [rows] = await db.query(
+            `UPDATE bundles SET name = ?, product_id = ?, price = ?, stock = ?, description = ? WHERE id = ?`,
+            [bundle.name, bundle.product_id, bundle.price, bundle.stock, bundle.description, id]
+        );
+        return rows;
+    } catch (error) {
+        throw new Error('Failed to update bundle');
+    }
 };
